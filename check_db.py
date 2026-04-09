c2291a69e74b05818b680cf7dbf2268e51a43102
dvankevich <dvankevich@gmail.com>
2026-04-09 19:47:56 +0300

    add .env

--- check_db.py
+++ check_db.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine, text
 import logging
+import os
+from dotenv import load_dotenv
 
 from connect import URL 
 
-logging.basicConfig(level=logging.INFO)
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
+logging.basicConfig(
+    level=getattr(logging, LOG_LEVEL.upper()), # Перетворюємо рядок "INFO" у константу logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
 logger = logging.getLogger(__name__)
 
 engine = create_engine(URL)
@@ -11,6 +18,6 @@ engine = create_engine(URL)
 try:
     with engine.connect() as connection:
         result = connection.execute(text("SELECT 1"))
-        logger.info(f"Підключення успішне! Результат: {result.fetchone()[0]}")
+        logger.info(f"Підключення успішне! Рівень логування: {LOG_LEVEL}")
 except Exception as e:
     logger.error(f"Помилка підключення: {e}")
--- connect.py
+++ connect.py
@@ -1,24 +1,28 @@
+import os
 import logging
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
+load_dotenv()
+
+URL = os.getenv("DATABASE_URL")
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
 logging.basicConfig(
-    level=logging.INFO,
+    level=getattr(logging, LOG_LEVEL.upper()), # Перетворюємо рядок "INFO" у константу logging.INFO
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
 
-URL = "postgresql://postgres:567234@localhost:5432/hw04"
-
-# echo=True щоб увімкнути логування SQLAlchemy
-engine = create_engine(URL, echo=False)
-
+# Створюємо engine
+engine = create_engine(URL)
 Session = sessionmaker(bind=engine)
 
 if __name__ == "__main__":
     try:
         with engine.connect() as connection:
             result = connection.execute(text("SELECT 1"))
-            logger.info(f"Підключення успішне! Результат: {result.fetchone()[0]}")
+            logger.info(f"Підключення успішне! Рівень логування: {LOG_LEVEL}")
     except Exception as e:
-        logger.error(f"Помилка підключення до бази даних: {e}")
+        logger.error(f"Помилка підключення: {e}")
